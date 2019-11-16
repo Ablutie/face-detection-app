82fad7a49ad6a34d06a9d4596cb966ed3576885a
Ablutie <mishu_navetistul@yahoo.com>
2019-11-16 23:38:14 +0200

    initial commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () => {
+    return (
+        <nav className="user">
+            <p className="link underline pa3 dim f3 black">Sign Out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
